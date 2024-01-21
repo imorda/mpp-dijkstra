2a11cf544d5a8d7d23527597ae4e85de2eb639f2
imorda <btimka1@gmail.com>
2024-01-22 06:44:36 +0700

    Task done.

--- src/Dijkstra.kt
+++ src/Dijkstra.kt
@@ -1,9 +1,14 @@
 package dijkstra
 
+import kotlinx.atomicfu.atomic
+import kotlinx.atomicfu.locks.reentrantLock
 import java.util.*
 import java.util.concurrent.Phaser
-import kotlin.Comparator
 import kotlin.concurrent.thread
+
+/**
+ * @author Belousov Timofey
+ */
 
 private val NODE_DISTANCE_COMPARATOR = Comparator<Node> { o1, o2 -> Integer.compare(o1!!.distance, o2!!.distance) }
 
@@ -13,29 +18,88 @@ fun shortestPathParallel(start: Node) {
     // The distance to the start node is `0`
     start.distance = 0
     // Create a priority (by distance) queue and add the start node into it
-    val q = PriorityQueue(workers, NODE_DISTANCE_COMPARATOR) // TODO replace me with a multi-queue based PQ!
+    val q = SuperPriorityQueue(workers, NODE_DISTANCE_COMPARATOR)
     q.add(start)
+    q.activeNodes.incrementAndGet()
     // Run worker threads and wait until the total work is done
     val onFinish = Phaser(workers + 1) // `arrive()` should be invoked at the end by each worker
     repeat(workers) {
         thread {
             while (true) {
-                // TODO Write the required algorithm here,
-                // TODO break from this loop when there is no more node to process.
-                // TODO Be careful, "empty queue" != "all nodes are processed".
-//                val cur: Node? = synchronized(q) { q.poll() }
-//                if (cur == null) {
-//                    if (workIsDone) break else continue
-//                }
-//                for (e in cur.outgoingEdges) {
-//                    if (e.to.distance > cur.distance + e.weight) {
-//                        e.to.distance = cur.distance + e.weight
-//                        q.addOrDecreaseKey(e.to)
-//                    }
-//                }
+                val cur: Node? = q.poll()
+                if (cur == null) {
+                    if (q.activeNodes.value == 0) break else continue
+                }
+                for (e in cur.outgoingEdges) {
+                    while (true) {
+                        val curDistance = cur.distance
+                        val curEDistance = e.to.distance
+                        if (curEDistance > curDistance + e.weight) {
+                            if (!e.to.casDistance(curEDistance, curDistance + e.weight)) continue
+                            q.activeNodes.incrementAndGet()
+                            q.add(e.to)
+                        }
+                        break
+                    }
+                }
+                q.activeNodes.decrementAndGet()
+                if (q.activeNodes.value < 0) throw IllegalStateException("WTF?")
             }
             onFinish.arrive()
         }
     }
     onFinish.arriveAndAwaitAdvance()
+}
+
+
+class SuperPriorityQueue<E>(workers: Int, private val comparator: Comparator<in E>) {
+    companion object {
+        private const val C = 3
+    }
+
+    val queues = Array(workers * C) { (PriorityQueue(1, comparator) to reentrantLock()) }
+    val activeNodes = atomic(0)
+
+    fun add(element: E) {
+        while (true) {
+            val (q, lock) = queues[queues.indices.random()]
+            if (!lock.tryLock()) continue
+            q.add(element)
+            lock.unlock()
+            return
+        }
+    }
+
+    fun poll(): E? {
+        while (true) {
+            val i1 = queues.indices.random()
+            var i2: Int
+            do {
+                i2 = queues.indices.random()
+            } while (i2 == i1)
+
+            val (q1, lock1) = queues[i1]
+            val (q2, lock2) = queues[i2]
+
+            val top1 = q1.peek()
+            val top2 = q2.peek()
+
+            val (targetQueue, targetLock) = if (top1 == null) {
+                q2 to lock2
+            } else if (top2 == null) {
+                q1 to lock1
+            } else if (comparator.compare(top1, top2) < 0) {
+                q1 to lock1
+            } else {
+                q2 to lock2
+            }
+
+            if (!targetLock.tryLock()) continue
+
+            val result = targetQueue.poll()
+
+            targetLock.unlock()
+            return result
+        }
+    }
 }
